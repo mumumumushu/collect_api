9df5b477a4f2a391d15d14efde7f775b3a0e563f
mumumumushu <mumumumushu@gmail.com>
2017-02-13 17:29:02 +0800

    new

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  apipie
+  
+ 	Settings.http.each do |item|
+  	get URI(item[1]["url"]).path, to: "http##{item[0].downcase.tr_s("::", "_")}"
+	end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
